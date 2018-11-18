d8e81b8856ffce451f7b817779f3752ff1ba876a
Denis Black <madaradess@gmail.com>
2018-11-18 22:35:30 +0500

    init commit

---
+++ Sources/App/Controllers/UserController.swift
@@ -0,0 +1,44 @@
+import Vapor
+
+final class UserController {
+
+    func list(_ req: Request) throws -> Future<View> {
+        let allUsers = User.query(on: req).all()
+
+        return allUsers.flatMap { users in
+            let data = ["userlist": users]
+            return try req.view().render("crud", data)
+        }
+    }
+
+    func create(_ req: Request) throws -> Future<Response> {
+        return try req.content.decode(User.self).flatMap { user in
+            return user.save(on: req).map { _ in
+                return req.redirect(to: "/users")
+            }
+        }
+    }
+
+    func update(_ req: Request) throws -> Future<Response> {
+        return try req.parameters.next(User.self).flatMap { user in
+            return try req.content.decode(UserForm.self).flatMap { userForm in
+                user.username = userForm.username
+                return user.save(on: req).map { _ in
+                    return req.redirect(to: "/users")
+                }
+            }
+        }
+    }
+
+    func delete(_ req: Request) throws -> Future<Response> {
+        return try req.parameters.next(User.self).flatMap { user in
+            return user.delete(on: req).map { _ in
+                return req.redirect(to: "/users")
+            }
+        }
+    }
+}
+
+struct UserForm: Content {
+    var username: String
+}
